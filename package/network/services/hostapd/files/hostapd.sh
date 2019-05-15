79f389b51d51d68a3c0d5d821de77887e2fbeae3
Robert Marko <robimarko@gmail.com>
2019-05-15 17:06:02 +0200

    hostapd: Add UCI support for RSSI controls
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -173,6 +173,9 @@ hostapd_common_add_bss_config() {
 	config_add_int maxassoc max_inactivity
 	config_add_boolean disassoc_low_ack isolate short_preamble
 
+	config_add_int signal_connect signal_stay signal_poll_time \
+	signal_drop_reason signal_strikes
+
 	config_add_int \
 		wep_rekey eap_reauth_period \
 		wpa_group_rekey wpa_pair_rekey wpa_master_rekey
@@ -278,7 +281,8 @@ hostapd_set_bss_options() {
 		iapp_interface eapol_version dynamic_vlan ieee80211w nasid \
 		acct_server acct_secret acct_port acct_interval \
 		bss_load_update_period chan_util_avg_period sae_require_mfp \
-		multi_ap multi_ap_backhaul_ssid multi_ap_backhaul_key
+		multi_ap multi_ap_backhaul_ssid multi_ap_backhaul_key \
+		signal_connect signal_stay signal_poll_time signal_drop_reason signal_strikes
 
 	set_default isolate 0
 	set_default maxassoc 0
@@ -296,6 +300,11 @@ hostapd_set_bss_options() {
 	set_default chan_util_avg_period 600
 	set_default utf8_ssid 1
 	set_default multi_ap 0
+	set_default signal_connect -128
+	set_default signal_stay -128
+	set_default signal_poll_time 10
+	set_default signal_drop_reason 3
+	set_default signal_strikes 3
 
 	append bss_conf "ctrl_interface=/var/run/hostapd"
 	if [ "$isolate" -gt 0 ]; then
@@ -317,6 +326,14 @@ hostapd_set_bss_options() {
 	append bss_conf "uapsd_advertisement_enabled=$uapsd" "$N"
 	append bss_conf "utf8_ssid=$utf8_ssid" "$N"
 	append bss_conf "multi_ap=$multi_ap" "$N"
+
+	if [ "$signal_connect" -gt -128 ]; then
+			append bss_conf "signal_connect=$signal_connect" "$N"
+			append bss_conf "signal_stay=$signal_stay" "$N"
+			append bss_conf "signal_poll_time=$signal_poll_time" "$N"
+			append bss_conf "signal_strikes=$signal_strikes" "$N"
+			append bss_conf "signal_drop_reason=$signal_drop_reason" "$N"
+	fi
 
 	[ "$tdls_prohibit" -gt 0 ] && append bss_conf "tdls_prohibit=$tdls_prohibit" "$N"
 
